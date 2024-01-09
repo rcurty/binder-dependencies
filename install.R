97592e378d91daf37a87c4651f8e32de331e01ce
Renata Curty <63265343+rcurty@users.noreply.github.com>
2024-01-08 16:14:01 -0800

    Update install.R

--- install.R
+++ install.R
@@ -1,1 +1,6 @@
-install.packages(c("bookdown", "tidyverse", "BayesFactor", "patchwork","usethis"))
+install.packages(c("tidyverse", "stringi", "bayesFactor", "patchwork", "usethis"),
+                 repos = "http://cran.r-project.org",
+                 dependencies = TRUE,
+                 type = "source",
+                 version = c("1.3.2", "1.7.8", "0.9.12-4.5", "1.1.3", "2.2.2"))
+
